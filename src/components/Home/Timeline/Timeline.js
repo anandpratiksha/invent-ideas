66a0bddabc3652ae661dbd841d703d2553a2bb51
anandpratiksha <apratiksha1137@gmail.com>
2021-09-14 00:11:36 +0530

    updates

--- src/components/Home/Timeline/Timeline.js
+++ src/components/Home/Timeline/Timeline.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Timeline.css'
+import './Timeline.scss'
 import step1 from '../../img/step1.png'
 import step2 from '../../img/step2.png'
 import step3 from '../../img/step3.png'
@@ -7,43 +7,49 @@ import step4 from '../../img/step4.png'
 
 function Timeline() {
     return (
-        <div className="submain">
-            <h1 className="description">How it Works</h1>
-            <div className="timeline">
-                <div className="top_row">
-                    <div className="center">
-                        <div >Step:1</div>
-                        <div >Post your ideas</div>
-                        <div >Innovator submit their ideas to our platform.</div>
-                    </div>
-                    <div className="center">
-                        <div >Step:3</div>
-                        <div >Connecting investor and innovator</div>
-                        <div >Being interested in an idea investor<br /> contacts innovator for furture process.</div>
-                    </div>
-
+        <div className="timeline">
+            <section className="ps-timeline-sec">
+                <div className="container-timeline">
+                    <ol className="ps-timeline">
+                        <li>
+                            <div className="ps-top">
+                                <p><h4>Step1</h4>Innovator submit their ideas to our platform.</p>
+                            </div>
+                            <div className="ps-bot">
+                                <h5>Post your ideas</h5>
+                            </div>
+                            <span className="ps-sp-top"><img className="timeline-img" src={step1} alt="" /></span>
+                        </li>
+                        <li>
+                            <div className="ps-bot">
+                                <p><h4>step2</h4>Investors select any ideas they are interested in.</p>
+                            </div>
+                            <div className="ps-top">
+                                <h5>Collection of ideas</h5>
+                            </div>
+                            <span className="ps-sp-bot"><img className="timeline-img" src={step2} alt="" /></span>
+                        </li>
+                        <li>
+                            <div className="ps-top">
+                                <p><h4>Step3</h4>Being interested in an idea investor contacts innovator for furture process.</p>
+                            </div>
+                            <div className="ps-bot">
+                                <h5>Connecting investor and innovator</h5>
+                            </div>
+                            <span className="ps-sp-top"><img className="timeline-img" src={step3} alt="" /></span>
+                        </li>
+                        <li>
+                            <div className="ps-bot">
+                                <p><h4>Step4</h4>Finally the product of investor and innovator is  now available in market.</p>
+                            </div>
+                            <div className="ps-top">
+                                <h5>Product in market</h5>
+                            </div>
+                            <span className="ps-sp-bot"><img className="timeline-img" src={step4} alt="" /></span>
+                        </li>
+                    </ol>
                 </div>
-                <div className="how_icon">
-                    <div className="box"><img src={step1} alt="" /></div>
-                    <div className="box"><img src={step3} alt="" /></div>
-                    <div className="box"><img src={step2} alt="" /></div>
-                    <div className="box"><img src={step4} alt="" /></div>
-
-                </div>
-                <div className="bottom_row">
-                    <div className="lower">
-                        <div >Step:2</div>
-                        <div >Collection of ideas</div>
-                        <div >Investors select any ideas they are interested in.</div>
-                    </div>
-                    <div className='lower'>
-                        <div >Step:4</div>
-                        <div >Product in market</div>
-                        <div >Finally the product of investor and <br />innovator is  now available in market.</div>
-                    </div>
-                </div>
-            </div>
-
+            </section>
         </div>
     )
 }
